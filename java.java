9b5b16ce83e938366f00576fce4f50cadad85ba3
calistawua <127809356+calistawua@users.noreply.github.com>
2024-04-01 11:57:11 +0800

    Add files via upload

---
+++ java.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihan;
+
+/**
+ *
+ * @author calista
+ * tglL:2024-04-01
+ */
+public class latihan {
+    public static void main(String[] args){
+        for (int i=1; i<=10; i++)
+        {
+            if(i==5)
+            {
+                continue;
+            }
+            else
+            {
+                System.out.println(i);
+            }
+        }
+    }
+    
+    
+    
+}
---
+++ switchcase.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan3;
+
+/**
+ *
+ * @author Calista
+ * tgl:2024-04-01
+ */
+public class switchcase {
+    
+    
+    public static void main(String[] args){
+        String nVal = "A";
+            
+        
+        switch (nVal){
+            case "A":
+                System.out.println("Nilai nVar adalah A");
+                break;
+            case "a":
+                System.out.println("Nilai nVar adalah a");
+                break;
+            case "B":
+                System.out.println("Nilai nVar adalah B");
+                break;
+            case "C":
+                System.out.println("Nilai nVar adalah C");
+                break;
+            default:
+                System.out.println("Nilai nVar adalah C");
+                break;
+                
+                    
+                 
+        }
+        
+    }
+    
+}
+
